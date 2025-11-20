5a178c1e44a41ee95fdc2e63ba25bee0f2bebceb
Thorium1764 <tt4289692@gmail.com>
2025-11-20 20:12:26 +0200

    started with the lexer in zig

--- src/knotzig/src/lexer.zig
+++ src/knotzig/src/lexer.zig
@@ -3,15 +3,22 @@ const std = @import("std");
 pub const TokenType = enum {
     IntLit,
     CharLit,
-    StringLit,
     FloatLit,
+    StringLit,
+    TypeLit,
     Plus,
-    Minus,
     Star,
+    Minus,
     FSlash,
-    BSlash,
-    Modulo,
-    Ampersand,
+    While,
+    OpenParen,
+    CloseParen,
+    OpenCurly,
+    CloseCurly,
+    Assign,
+    If,
+    Else,
+    Ident,
 };
 
 pub const Value = union(enum){
@@ -19,6 +26,7 @@ pub const Value = union(enum){
     Float: f64,
     Char: u8,
     String: []const u8,
+    Type: TypeV,
 };
 
 pub const Token = struct {
@@ -27,7 +35,50 @@ pub const Token = struct {
     value: Value,
 };
 
+pub const TypeV = enum {
+    Int,
+    Char,
+    String,
+    Float,
+};
+
+pub const src = struct {
+    src: []const u8,
+    index: i32,
+
+
+    pub fn tokenize(self: src) ![]const Token {
+        
+
+
+
+        const tokens: [1]Token = .{Token{
+            .toktype = TokenType.IntLit,
+            .line = 1,
+            .value = Value{ .String = src }
+        }};
+        return tokens[0..];
+    }
+
+    fn peek(self: src, offset: i32) u8
+    {
+        if (self.index + offset >= self.src.len)
+            return 0;
+        return self.src[self.index + offset];
+    }
+
+    fn consume(self: src) u8
+    {
+        self.index += 1;
+        return self.src[self.index-1];
+    }
+};
+
 pub fn tokenize(src: []const u8) ![]const Token {
+    
+
+
+
     const tokens: [1]Token = .{Token{
         .toktype = TokenType.IntLit,
         .line = 1,
@@ -35,3 +86,5 @@ pub fn tokenize(src: []const u8) ![]const Token {
     }};
     return tokens[0..];
 }
+
+fn peek(offset: int = 0)
